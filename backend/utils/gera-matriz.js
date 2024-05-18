51463a18d901d5b77a7757567ca9100bb9a366f3
lucas-2000 <lucasmarchesoni620@gmail.com>
2024-05-18 20:14:04 -0300

    arrumado função gera matriz

--- backend/utils/gera-matriz.js
+++ backend/utils/gera-matriz.js
@@ -1,14 +1,9 @@
 export function geraMatriz(rows, columns, matrixValues) {
-  const matrix = Array.from(
-    {
-      length: columns,
-    },
-    () => new Array(rows).fill(0)
-  );
+  const matrix = Array.from({ length: rows }, () => new Array(columns).fill(0));
 
   let k = 0;
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < columns; j++) {
       matrix[i][j] = matrixValues[k];
       k++;
     }
